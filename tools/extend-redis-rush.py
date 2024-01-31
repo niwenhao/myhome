339c0cc80cb9ac2f564ffedf9d0d3f771441589e
Your Name <you@example.com>
2024-01-31 16:17:21 +0900

    add require

---
+++ tools/extend-redis-rush.py
@@ -0,0 +1,3 @@
+# read a log file that each line is composed with "start_time podname process_id durition access_per_second"
+# it means has durition seconds with the throughput of access_per_second since the start_time
+# I want to extends each line to durition lines same as the origion line but repire the start_time to the right vale.
