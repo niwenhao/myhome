ba09760bc95a090a9872d3f7c37fd8449ccd39a5
Your Name <you@example.com>
2024-01-31 16:28:55 +0900

    Generated by cody

--- tools/extend-redis-rush.py
+++ tools/extend-redis-rush.py
@@ -1,3 +1,25 @@
-# read a log file that each line is composed with "start_time podname process_id durition access_per_second"
+# read a log file that each line is composed with "start_time(formated with hh:mm:ss) podname process_id durition access_per_second"
 # it means has durition seconds with the throughput of access_per_second since the start_time
 # I want to extends each line to durition lines same as the origion line but repire the start_time to the right vale.
+
+import sys
+import datetime
+
+def extend_log_lines(log_lines):
+    extended_lines = []
+    for line in log_lines:
+        start_time, podname, process_id, duration, access_per_second = line.split()
+        start_time = datetime.datetime.strptime(start_time, "%H:%M:%S")
+        for i in range(int(duration)):
+            new_start_time = start_time + datetime.timedelta(seconds=i)
+            new_start_time_str = new_start_time.strftime("%H:%M:%S")
+            new_line = f"{new_start_time_str} {podname} {process_id} {duration} {access_per_second}"
+            extended_lines.append(new_line)
+    return extended_lines
+
+
+for line in sys.stdin:
+    extended_lines = extend_log_lines([line])  
+    for extended_line in extended_lines:
+        print(extended_line)
+
